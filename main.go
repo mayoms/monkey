d7d9e5e7c177b0ce05d070606da6d82e6c5aa2c2
mmayo <mmayo@newrelic.com>
2017-03-29 16:47:32 -0700

    removed os.in from main

--- main.go
+++ main.go
@@ -4,15 +4,9 @@ import (
 	"fmt"
 	"monkey/repl"
 	"os"
-	"os/user"
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Hello %s! This is the monkey programming language!\n", user.Username)
-	fmt.Printf("Feel free to try out some commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+	fmt.Println("Monkey programming language REPL\n")
+	repl.Start(os.Stdout)
 }
--- repl/repl.go
+++ repl/repl.go
@@ -1,36 +1,51 @@
 package repl
 
 import (
-	"bufio"
-	"fmt"
 	"io"
 	"monkey/lexer"
 	"monkey/parser"
+	"os"
+	"path/filepath"
+
+	"github.com/peterh/liner"
 )
 
 const PROMPT = ">> "
 
-func Start(in io.Reader, out io.Writer) {
-	scanner := bufio.NewScanner(in)
+func Start(out io.Writer) {
+	history := filepath.Join(os.TempDir(), ".monkey_history")
+	l := liner.NewLiner()
+	defer l.Close()
+
+	l.SetCtrlCAborts(true)
+
+	if f, err := os.Open(history); err == nil {
+		l.ReadHistory(f)
+		f.Close()
+	}
 
 	for {
-		fmt.Printf(PROMPT)
-		scanned := scanner.Scan()
-		if !scanned {
-			return
-		}
-		line := scanner.Text()
-		l := lexer.New(line)
-		p := parser.New(l)
+		if line, err := l.Prompt(PROMPT); err == nil {
+			if line == "exit" {
+				if f, err := os.Create(history); err == nil {
+					l.WriteHistory(f)
+					f.Close()
+				}
+				break
+			}
+			lex := lexer.New(line)
+			p := parser.New(lex)
 
-		program := p.ParseProgram()
-		if len(p.Errors()) != 0 {
-			printParserErrors(out, p.Errors())
-			continue
-		}
+			program := p.ParseProgram()
+			if len(p.Errors()) != 0 {
+				printParserErrors(out, p.Errors())
+				continue
+			}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+			io.WriteString(out, program.String())
+			io.WriteString(out, "\n")
+			l.AppendHistory(line)
+		}
 	}
 }
 
